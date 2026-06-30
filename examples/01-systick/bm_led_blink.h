b3a5816020c1803b5f978194a0c92d0d97261e43
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-06-30 11:14:17 +0700

    [RENAME] example 01 to 01-systick

--- examples/01-systick/bm_led_blink.h
+++ examples/01-systick/bm_led_blink.h
@@ -3,9 +3,18 @@
 
 #include <stdint.h>
 
+typedef struct
+{
+	volatile uint32_t RCC_AHBENR;
+	volatile uint32_t GPIOB_MODER;
+	volatile uint32_t GPIOB_BSRR;
+} bm_led_blink_gpio_t;
+
 #define RCC_AHBENR (*(volatile uint32_t*)0x4002381C)
 #define GPIOB_MODER (*(volatile uint32_t*)0x40020400)
 #define GPIOB_BSRR (*(volatile uint32_t*)0x40020418)
+
+#define GPIOB ((bm_led_blink_t*)0x40020400)
 
 #define LED_PIN 8
 
