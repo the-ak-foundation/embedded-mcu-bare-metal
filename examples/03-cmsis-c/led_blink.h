5a9895b8ff3d15ee8dcbae593a86cda4cf89a5e4
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-08 00:09:34 +0700

    [UPDATE] using block comment /* */ in 03-cmsis-c led_blink.h

--- examples/03-cmsis-c/led_blink.h
+++ examples/03-cmsis-c/led_blink.h
@@ -1,8 +1,10 @@
 #ifndef __LED_BLINK_H__
 #define __LED_BLINK_H__
 
-// One include from ST replaces all the struct + define we hand-wrote in 02.
-// Picks the right chip header via the -DSTM32L151xB flag in the Makefile.
+/*
+ * One include from ST replaces all the struct + define we hand-wrote in 02.
+ * Picks the right chip header via the -DSTM32L151xB flag in the Makefile.
+ */
 #include "stm32l1xx.h"
 
 #define LED_PIN   8
