91e996b0604e57b0a4558f2198308061a094dc38
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-07 23:38:42 +0700

    [DOCS] plain up 04-hal-c led_blink.h comment

--- examples/04-hal-c/led_blink.h
+++ examples/04-hal-c/led_blink.h
@@ -1,8 +1,8 @@
 #ifndef __LED_BLINK_H__
 #define __LED_BLINK_H__
 
-// stm32l1xx_hal.h pulls in stm32l1xx.h (chip register maps from CMSIS-Device)
-// plus every HAL module enabled in stm32l1xx_hal_conf.h.
+// stm32l1xx_hal.h includes stm32l1xx.h (chip register maps from CMSIS-Device)
+// and every HAL module enabled in stm32l1xx_hal_conf.h.
 #include "stm32l1xx_hal.h"
 
 #define LED_PORT GPIOB
